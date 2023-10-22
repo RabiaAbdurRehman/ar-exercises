3b25fe8f2fff89c3fda3e4044137b33fb0b8963a
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-10-22 08:41:29 -0600

    Exercise 5 is completed by adding average, count, where, and sum

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for the entire commpany is #{total_revenue}"
+
+average = Store.average(:annual_revenue)
+puts "average of annual revenue of the company is #{average}"
+
+store_with_condition = Store.where('annual_revenue >= ?', 1000000).count
+puts "Store with conditions are #{store_with_condition}"
